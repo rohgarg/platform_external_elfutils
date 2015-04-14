33628bf505f1d1c1b645dc9ce37438243569d0db
Rohan Garg <rohgarg@ccs.neu.edu>
2015-04-14 17:43:47 -0400

    compile strings for Android

--- jni/0.153/src/Android.mk
+++ jni/0.153/src/Android.mk
@@ -105,3 +105,37 @@ LOCAL_MODULE := readelf
 LOCAL_STATIC_LIBRARIES := libdwfl libdw libebl libebp_pic libargp libobstack libeu libasm libelf
 
 include $(BUILD_EXECUTABLE)
+
+##############################################
+# Build strings
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -Wall -DHAVE_CONFIG_H
+
+LOCAL_C_INCLUDES := \
+        $(LOCAL_PATH)/.. \
+        $(LOCAL_PATH)/../libdwfl \
+        $(LOCAL_PATH)/../lib \
+        $(LOCAL_PATH)/../libdw \
+        $(LOCAL_PATH)/../libasm \
+        $(LOCAL_PATH)/../libebl \
+        $(LOCAL_PATH)/../libelf
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../bionic-fixup
+
+LOCAL_CFLAGS += -include $(LOCAL_PATH)/../bionic-fixup/AndroidFixup.h
+
+LOCAL_CFLAGS += -DHAVE_CONFIG_H -std=gnu99 -Werror
+
+# to suppress the "pointer of type ‘void *’ used in arithmetic" warning
+LOCAL_CFLAGS += -Wno-pointer-arith
+
+LOCAL_SRC_FILES := strings.c
+
+LOCAL_MODULE := strings
+
+LOCAL_STATIC_LIBRARIES := libdwfl libdw libebl libargp libobstack libeu libasm libelf
+
+include $(BUILD_EXECUTABLE)
