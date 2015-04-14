0db26b2b1b96ab277d07d8aae3b197090426bc4f
Rohan Garg <rohgarg@ccs.neu.edu>
2015-04-14 17:12:57 -0400

    add a dummy langinfo.h header

---
+++ jni/0.153/bionic-fixup/langinfo.h
@@ -0,0 +1,9 @@
+#ifndef ELFUTILS_LANGINFO_H
+#define ELFUTILS_LANGINFO_H
+
+/* workarounds for langinfo.h */
+#define nl_langinfo
+#define YESSTR "YES"
+#define NOSTR  "NO"
+
+#endif /* ELFUTILS_LANGINFO_H */
