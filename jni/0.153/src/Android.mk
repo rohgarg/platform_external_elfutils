3f823944aaee8c31e1e0f94637463f9ba528c08e
Rohan Garg <rohgarg@ccs.neu.edu>
2015-04-14 17:02:56 -0400

    compile objdump for Android

--- jni/0.153/src/Android.mk
+++ jni/0.153/src/Android.mk
@@ -34,3 +34,36 @@ LOCAL_MODULE := nm
 LOCAL_STATIC_LIBRARIES := libdwfl libdw libebl libargp libobstack libeu libasm libelf
 
 include $(BUILD_EXECUTABLE)
+
+##############################################
+# Build objdump
+#
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS := -Wall -DHAVE_CONFIG_H
+
+LOCAL_C_INCLUDES := \
+        $(LOCAL_PATH)/.. \
+        $(LOCAL_PATH)/../lib \
+        $(LOCAL_PATH)/../libasm \
+        $(LOCAL_PATH)/../libdw \
+        $(LOCAL_PATH)/../libebl \
+        $(LOCAL_PATH)/../libelf
+
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../bionic-fixup
+
+LOCAL_CFLAGS += -include $(LOCAL_PATH)/../bionic-fixup/AndroidFixup.h
+
+LOCAL_CFLAGS += -DHAVE_CONFIG_H -std=gnu99 -Werror
+
+# to suppress the "pointer of type ‘void *’ used in arithmetic" warning
+LOCAL_CFLAGS += -Wno-pointer-arith
+
+LOCAL_SRC_FILES := objdump.c
+
+LOCAL_MODULE := objdump
+
+LOCAL_STATIC_LIBRARIES := libdwfl libdw libebl libebp_pic libargp libeu libasm libelf
+
+include $(BUILD_EXECUTABLE)
